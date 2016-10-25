eb4aa4909e6108a84b7a5a67a3bcc9cbbe15ac0f
aaronkatz <aaronkatzaj@hotmail.com>
2016-10-25 15:08:49 -0400

    Quench It, Quentin
    Classic Arcade Game used to teach teens how to code

---
+++ Fireman/Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class MainForm
+    Dim cInGame As Boolean
+    Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Left Then
+            mHose.pSpeed = -10
+        End If
+        If e.KeyCode = Keys.Right Then
+            mHose.pSpeed = 10
+        End If
+        If e.KeyCode = Keys.Space Then
+            shootWater()
+        End If
+    End Sub
+    Private Sub MainForm_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If e.KeyCode = Keys.Left Then
+            mHose.pSpeed = 0
+        End If
+        If e.KeyCode = Keys.Right Then
+            mHose.pSpeed = 0
+        End If
+        If e.KeyCode = Keys.Enter Then
+            cInGame = True
+            loadGame()
+        End If
+    End Sub
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        loadBackground()
+
+        cInGame = False
+
+        Me.MaximumSize = New Size(mBackground.bWidth + 16, mBackground.bHeight + 38)
+        Me.MinimumSize = New Size(mBackground.bWidth + 16, mBackground.bHeight + 38)
+
+        Me.WindowState = FormWindowState.Maximized
+
+        Me.Height = mBackground.bHeight + 38
+        Me.Width = mBackground.bWidth + 16
+
+        onScreen.Top = mBackground.bLocation.X
+        onScreen.Left = mBackground.bLocation.Y
+        onScreen.Height = mBackground.bHeight
+        onScreen.Width = mBackground.bWidth
+
+
+    End Sub
+    Private Sub loadGame()
+        gameTime = 0
+        mScore = 0
+        makeHose()
+        loadPics()
+        mWaterDrops.Clear()
+
+        For i As Integer = 0 To 4
+            randomFireMaker()
+        Next i
+        GameTimer.Start()
+
+    End Sub
+    Public Sub makeHose()
+        mHose.pPic = Image.FromFile(IO.Path.GetDirectoryName(Application.ExecutablePath) & "\Pics\nozzle.png")
+        mHose.pSpeed = 0
+        mHose.pTimeSinceLastShot = 50
+        mHose.pLocation.X = 375
+        mHose.pLocation.Y = Me.Height - 38 - mHose.pPic.Height
+    End Sub
+    Private Sub updateScreen()
+        mG = onScreen.CreateGraphics
+        mOffG = Graphics.FromImage(mOffScreenImage)
+        drawBackground()
+
+
+        If cInGame = True Then
+            Dim index As Integer
+            Dim jndex As Integer
+            If winState() = False And loseState() = False Then
+                For index = 0 To mFires.GetLength(0) - 1
+                    For jndex = 0 To mFires.GetLength(1) - 1
+                        If (mFires(index, jndex).aActive) Then
+                            drawAnimatedSprite(mFires(index, jndex))
+                            updateFire(mFires(index, jndex))
+                        End If
+                    Next jndex
+                Next index
+                Dim wndex As Integer
+                For wndex = 0 To mWaterDrops.Count - 1
+                    If (mWaterDrops(wndex).aActive) Then
+                        drawAnimatedSprite(mWaterDrops(wndex))
+                        updateWater(mWaterDrops(wndex))
+                    End If
+                Next wndex
+                drawPlayer()
+                updatePlayer()
+            End If
+
+
+
+            mOffG.DrawString("Score: " + mScore.ToString, New Font("Arial", 12, FontStyle.Bold), New SolidBrush(Color.Red), 0, 0)
+            If winState() Then
+                mOffG.DrawString("You Win", New Font("Arial", 50, FontStyle.Bold), New SolidBrush(Color.Green), 300, 275)
+            End If
+            If loseState() Then
+                mOffG.DrawString("You Lose", New Font("Arial", 50, FontStyle.Bold), New SolidBrush(Color.Red), 300, 275)
+            End If
+            mOffG.DrawString("Press ENTER to restart", New Font("Arial", 12, FontStyle.Bold), New SolidBrush(Color.Red), 600, 0)
+        Else
+            mOffG.DrawString("Press ENTER to start", New Font("Arial", 12, FontStyle.Bold), New SolidBrush(Color.Red), 600, 0)
+            mOffG.DrawString("Quench it," & vbCrLf & "Quentin", New Font("Arial", 40, FontStyle.Bold), New SolidBrush(Color.Blue), 525, 225)
+        End If
+        mG.DrawImage(mOffScreenImage, 0, 0)
+        mG.Dispose()
+        mOffG.Dispose()
+    End Sub
+    Private Sub GameTimer_Tick(sender As Object, e As EventArgs) Handles GameTimer.Tick
+        updateScreen()
+
+        If winState() = False And loseState() = False Then
+
+            gameTime += 1
+            checkTouching()
+            spawnFire()
+            removeOld()
+        End If
+
+    End Sub
+    Public Function winState()
+        For index As Integer = 0 To mFires.GetLength(0) - 1
+            For jndex As Integer = 0 To mFires.GetLength(1) - 1
+                If mFires(index, jndex).aActive = True Then
+                    Return False
+                End If
+            Next jndex
+        Next index
+        Return True
+    End Function
+    Public Function loseState()
+        For index As Integer = 0 To mFires.GetLength(0) - 1
+            For jndex As Integer = 0 To mFires.GetLength(1) - 1
+                If mFires(index, jndex).aActive = False Then
+                    Return False
+                End If
+            Next jndex
+        Next index
+        Return True
+    End Function
+    Private Sub checkTouching()
+        For index As Integer = 0 To mFires.GetLength(0) - 1
+            For jndex As Integer = 0 To mFires.GetLength(1) - 1
+                For wndex As Integer = 0 To mWaterDrops.Count - 1
+                    If touching(mFires(index, jndex), mWaterDrops(wndex)) = True Then
+                        Dim temp As AnimatedSprite = mWaterDrops(wndex)
+                        temp.aActive = False
+                        mScore += 10
+                        mWaterDrops(wndex) = temp
+                        mFires(index, jndex).aActive = False
+                    End If
+                Next wndex
+            Next jndex
+        Next index
+    End Sub
+End Class
---
+++ Fireman/SpriteStuff.vb
@@ -0,0 +1,151 @@
+﻿Module SpriteStuff
+    Structure AnimatedSprite
+        Dim aPic As Image
+        Dim aLocation As Point
+        Dim aActive As Boolean
+        Dim aTotalFrames As Integer
+        Dim aCurrentFrame As Integer
+    End Structure
+    Structure Player
+        Dim pPic As Image
+        Dim pLocation As Point
+        Dim pSpeed As Single
+        Dim pTimeSinceLastShot As Integer
+    End Structure
+    Public mWaterDrops As List(Of AnimatedSprite) = New List(Of AnimatedSprite)
+    Public mRemovableDrops As List(Of AnimatedSprite) = New List(Of AnimatedSprite)
+    Public mFires(4, 9) As AnimatedSprite
+    Public mHose As Player
+    Public mScore As Integer
+    Public gameTime As Integer
+    Dim mFireFiles(4) As Image
+    Dim mWaterFiles(1) As Image
+    Public Sub loadPics()
+        Dim wndex As Integer
+        For wndex = 0 To 1
+            mWaterFiles(wndex) = Image.FromFile(IO.Path.GetDirectoryName(Application.ExecutablePath) & "\Pics\waters" & wndex & ".png")
+        Next wndex
+        makeFires()
+    End Sub
+    Public Function touching(ByVal guy1 As AnimatedSprite, ByVal guy2 As AnimatedSprite)
+        If guy1.aActive = True And guy2.aActive = True Then
+            If guy1.aLocation.X < guy2.aLocation.X + guy2.aPic.Width And guy1.aLocation.X + guy1.aPic.Width > guy2.aLocation.X Then
+                If guy1.aLocation.Y < guy2.aLocation.Y + guy2.aPic.Height And guy1.aLocation.Y + guy1.aPic.Height > guy2.aLocation.Y Then
+                    Return True
+                End If
+            End If
+        End If
+        Return False
+    End Function
+    Public Sub removeOld()
+        For Each water As AnimatedSprite In mWaterDrops
+            If water.aActive = False Then
+                mRemovableDrops.Add(water)
+            End If
+        Next water
+        For Each rwater As AnimatedSprite In mRemovableDrops
+            mWaterDrops.Remove(rwater)
+        Next rwater
+        mRemovableDrops.Clear()
+    End Sub
+    Public Sub makeWater()
+        Dim water As New AnimatedSprite
+        water.aTotalFrames = 2
+        water.aCurrentFrame = 0
+        water.aLocation.X = mHose.pLocation.X + 5
+        water.aLocation.Y = mHose.pLocation.Y
+        water.aActive = True
+        water.aPic = mWaterFiles(water.aCurrentFrame)
+        mWaterDrops.Add(water)
+    End Sub
+    Public Sub shootWater()
+        If mHose.pTimeSinceLastShot > 7 Then
+
+            makeWater()
+            mHose.pTimeSinceLastShot = 0
+        End If
+    End Sub
+    Public Sub makeFires()
+        Dim fndex As Integer
+        For fndex = 0 To 4
+            mFireFiles(fndex) = Image.FromFile(IO.Path.GetDirectoryName(Application.ExecutablePath) & "\Pics\fires" & fndex & ".png")
+        Next fndex
+        Dim index As Integer
+        Dim jndex As Integer
+        For index = 0 To mFires.GetLength(0) - 1
+            For jndex = 0 To mFires.GetLength(1) - 1
+                mFires(index, jndex).aCurrentFrame = 0
+                mFires(index, jndex).aTotalFrames = 5
+                mFires(index, jndex).aActive = False
+                mFires(index, jndex).aLocation.X = 280 + 44 * index
+                mFires(index, jndex).aLocation.Y = 38 + 46 * jndex
+                mFires(index, jndex).aPic = mFireFiles(mFires(index, jndex).aCurrentFrame)
+            Next jndex
+        Next index
+    End Sub
+    Public Sub drawAnimatedSprite(ByVal guy As AnimatedSprite)
+        mOffG.DrawImage(guy.aPic, guy.aLocation.X, guy.aLocation.Y)
+    End Sub
+    Public Sub drawPlayer()
+        mOffG.DrawImage(mHose.pPic, mHose.pLocation.X, mHose.pLocation.Y)
+    End Sub
+    Public Sub updatePlayer()
+        mHose.pTimeSinceLastShot += 1
+        mHose.pLocation.X += mHose.pSpeed
+        If mHose.pLocation.X <= 0 Then
+            mHose.pLocation.X = 0
+        ElseIf mHose.pLocation.X >= 800 - mHose.pPic.Width Then
+            mHose.pLocation.X = 800 - mHose.pPic.Width
+        End If
+    End Sub
+    Public Sub updateFire(ByRef guy As AnimatedSprite)
+        guy.aCurrentFrame += 1
+        If (guy.aCurrentFrame >= guy.aTotalFrames) Then
+            guy.aCurrentFrame = 0
+        End If
+        guy.aPic = mFireFiles(guy.aCurrentFrame)
+    End Sub
+    Public Sub updateWater(ByRef guy As AnimatedSprite)
+        guy.aCurrentFrame += 1
+        If (guy.aCurrentFrame >= guy.aTotalFrames) Then
+            guy.aCurrentFrame = 0
+        End If
+        guy.aPic = mWaterFiles(guy.aCurrentFrame)
+        guy.aLocation.Y -= 15
+        If guy.aLocation.Y + guy.aPic.Height < 0 Then
+            mScore -= 3
+            guy.aActive = False
+        End If
+    End Sub
+    Public Sub randomFireMaker()
+        Dim index As Integer
+        Dim jndex As Integer
+        Dim counter As Integer
+        counter = 0
+        For index = 0 To mFires.GetLength(0) - 1
+            For jndex = 0 To mFires.GetLength(1) - 1
+                If mFires(index, jndex).aActive = True Then
+                    counter += 1
+                End If
+
+            Next jndex
+        Next index
+        If counter = 50 Then
+            Return
+        End If
+        Dim x As Integer
+        Dim y As Integer
+        Randomize()
+        x = Int(Rnd() * 5)
+        y = Int(Rnd() * 10)
+        If mFires(x, y).aActive = True Then
+            randomFireMaker()
+        End If
+        mFires(x, y).aActive = True
+    End Sub
+    Public Sub spawnFire()
+        If gameTime Mod 20 = 0 Then
+            randomFireMaker()
+        End If
+    End Sub
+End Module
